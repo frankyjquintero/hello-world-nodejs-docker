596645b10c7774f665f247155f40aa139c22c5b2
FrankyJQuintero <frankyjquintero@unicesar.edu.co>
2019-02-06 00:14:08 -0500

    Update dependencies

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1> Hello world NodejS from Docker </h1> \n');
+  res.send('<h1> Hello world NodejS from Docker - FrankyJQuintero</h1> \n');
 });
 
 app.listen(PORT, HOST);
